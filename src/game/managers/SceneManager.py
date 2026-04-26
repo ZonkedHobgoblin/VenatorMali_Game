e88b524892e58530f2792c4b26d7d4da6a4e4c11
Iselin <iselin.rolness03@gmail.com>
2026-04-26 15:02:18 +0100

    Added level.py from the prototype

--- src/game/managers/SceneManager.py
+++ src/game/managers/SceneManager.py
@@ -1,8 +1,10 @@
 
+from .LevelManager import LevelManager
+
 
 class SceneManager:
     def __init__(self):
-        self.level_manager = None
+        self.level_manager = LevelManager(["lv1"])
         self.physics_manager = None
         self.camera_manager = None
         self.ui_manager = None
