143469a2965369917c8332aa4237276eaec4b470
Iselin <iselin.rolness03@gmail.com>
2026-04-30 15:37:27 +0100

    added images and deleted previous images

---
+++ assets/images/free.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="free" tilewidth="64" tileheight="64" spacing="2" tilecount="15" columns="5">
+ <image source="../../../../Downloads/free.png" width="384" height="256"/>
+</tileset>
