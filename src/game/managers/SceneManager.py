786ba2198ad310671b8672f008b84fa30a5e1c6e
Iselin <iselin.rolness03@gmail.com>
2026-05-13 14:13:44 +0100

    trying to add  level1

--- src/game/managers/SceneManager.py
+++ src/game/managers/SceneManager.py
@@ -4,7 +4,7 @@ from .LevelManager import LevelManager
 
 class SceneManager:
     def __init__(self):
-        self.level_manager = LevelManager(["lv1"])
+        self.level_manager = None
         self.physics_manager = None
         self.camera_manager = None
         self.ui_manager = None
