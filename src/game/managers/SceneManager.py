e2f72a96c9d9d2acd57e74ced992c4caae49d062
Iselin <iselin.rolness03@gmail.com>
2026-05-15 16:57:16 +0100

    adding files

--- src/game/managers/SceneManager.py
+++ src/game/managers/SceneManager.py
@@ -4,7 +4,7 @@ from .LevelManager import LevelManager
 
 class SceneManager:
     def __init__(self):
-        self.level_manager = None
+        self.level_manager = LevelManager()
         self.physics_manager = None
         self.camera_manager = None
         self.ui_manager = None
