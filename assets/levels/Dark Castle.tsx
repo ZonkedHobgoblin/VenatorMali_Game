75f68f2bf35115050fc6ad621eae0e780519fdb1
F7eddie <fthurstans@gmail.com>
2026-05-30 15:11:46 +0100

    Small Upgrade

--- assets/levels/Dark Castle.tsx
+++ assets/levels/Dark Castle.tsx
@@ -1,5 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.12.1" name="Dark Castle" tilewidth="16" tileheight="16" tilecount="242" columns="22">
+ <properties>
+  <property name="damdage" type="int" value="50"/>
+  <property name="hazzard" type="bool" value="true"/>
+ </properties>
  <image source="../images/dark_castle_tileset.png" width="358" height="179"/>
  <tile id="23">
   <properties>
@@ -44,6 +48,12 @@
  <tile id="69">
   <properties>
    <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="81">
+  <properties>
+   <property name="damage" type="int" value="75"/>
+   <property name="hazard" type="bool" value="true"/>
   </properties>
  </tile>
 </tileset>
