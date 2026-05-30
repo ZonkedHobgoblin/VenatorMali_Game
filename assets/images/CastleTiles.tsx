2fa1c2b18cd7a36bfed47caf7363263f2fcd1f5c
F7eddie <fthurstans@gmail.com>
2026-05-30 13:41:51 +0100

    Small Push - Older Version

--- assets/images/CastleTiles.tsx
+++ assets/images/CastleTiles.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.12.1" name="CastleTiles" tilewidth="16" tileheight="16" tilecount="224" columns="16">
- <image source="../images/CastleTiles.png" width="256" height="224"/>
+ <image source="../../../../../images/CastleTiles.png" width="256" height="224"/>
  <tile id="102">
   <properties>
    <property name="solid" type="bool" value="true"/>
--- assets/images/Dark Castle.tsx
+++ assets/images/Dark Castle.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.12.1" name="Dark Castle" tilewidth="16" tileheight="16" tilecount="242" columns="22">
- <image source="../images/dark_castle_tileset.png" width="358" height="179"/>
+ <image source="..\..\..\..\..\images\dark_castle_tileset.png" width="358" height="179"/>
  <tile id="23">
   <properties>
    <property name="solid" type="bool" value="true"/>
