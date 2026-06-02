86c66aeb9ec02ed6346945dab0ce77a8ba4550aa
Iselin <iselin.rolness03@gmail.com>
2026-06-02 14:32:48 +0100

    level editing

--- assets/levels/Dark_Castle.tsx
+++ assets/levels/Dark_Castle.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.12.1" name="Dark_Castle" tilewidth="16" tileheight="16" tilecount="242" columns="22">
- <image source="../../../../../../Downloads/dark_castle_tileset.png" width="358" height="179"/>
+ <image source="../images/dark_castle_tileset.png" width="358" height="179"/>
  <tile id="24">
   <properties>
    <property name="Solid" type="bool" value="true"/>
